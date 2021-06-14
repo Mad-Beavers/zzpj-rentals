9049a196efdd92c8dd7e4e918b510a7946b03f24
Cactus-7 <mat-wojt@hotmail.com>
2021-06-14 20:48:18 +0200

    RentMapper fix.

--- src/main/java/com/rentalhub/mappers/RentMapper.java
+++ src/main/java/com/rentalhub/mappers/RentMapper.java
@@ -1,14 +1,33 @@
 package com.rentalhub.mappers;
 
 import com.rentalhub.dto.RentDto;
+import com.rentalhub.model.Client;
 import com.rentalhub.model.Rent;
+import com.rentalhub.model.Vehicle;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface RentMapper {
 
+    @Mapping(source = "client", target = "login", qualifiedByName = "ClientToLogin")
+    @Mapping(source = "rentedVehicle", target = "vin", qualifiedByName = "VehicleToVin")
     RentDto toRentDto(Rent rent);
+
+    @Mapping(source = "client", target = "login", qualifiedByName = "ClientToLogin")
+    @Mapping(source = "rentedVehicle", target = "vin", qualifiedByName = "VehicleToVin")
     List<RentDto> toRentDtoList(List<Rent> rents);
+
+    @Named("ClientToLogin")
+    default String clientToLogin(Client client) {
+        return client.getLogin();
+    }
+
+    @Named("VehicleToVin")
+    default String vehicleToVin(Vehicle vehicle) {
+        return vehicle.getVin();
+    }
 }
