c74fcb3c1fae1f81077b3b438444ffa18067e2e5
Cactus-7 <mat-wojt@hotmail.com>
2021-06-14 03:40:17 +0200

    ExchangeRatesService improvements.

--- src/main/java/com/rentalhub/currencyService/ExchangeRatesService.java
+++ src/main/java/com/rentalhub/currencyService/ExchangeRatesService.java
@@ -10,8 +10,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Map;
+
 @Component
-public class ExchangeRatesReceiver {
+public class ExchangeRatesService {
 
     @Value("${api.key}")
     private String apiKey;
@@ -19,12 +23,23 @@ public class ExchangeRatesReceiver {
     @Value("${currency.api.uri}")
     private String apiURI;
 
+    private final RestTemplate restTemplate;
+
     @Autowired
-    private RestTemplate restTemplate;
+    public ExchangeRatesService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public double getExchangeRate(AcceptedCurrencies currency) throws CurrencyServiceException {
+        if (currency == AcceptedCurrencies.PLN) {
+            return 1.0;
+        }
+
         try {
-            return getResponse().getBody().getRates().get(currency);
+            Map<AcceptedCurrencies, Double> response = getResponse().getBody().getRates();
+            double currencyToEUR = response.get(currency);
+            double plnToEUR = response.get(AcceptedCurrencies.PLN);
+            return round(currencyToEUR / plnToEUR, 2);
         } catch (CurrencyClientErrorException | CurrencyServerErrorException | NullPointerException e) {
             throw new CurrencyServiceException("Unable to receive exchange rates", e);
         }
@@ -35,5 +50,13 @@ public class ExchangeRatesReceiver {
                 StringUtils.arrayToCommaDelimitedString(AcceptedCurrencies.values());
 
         return restTemplate.getForEntity(uri, ExchangeRatesResponse.class);
+    }
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
